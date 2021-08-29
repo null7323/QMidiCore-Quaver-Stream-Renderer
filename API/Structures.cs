82348584cf6f084cec62c6db270d09667af6450c
fnull601 <56241863+fnull601@users.noreply.github.com>
2021-08-29 15:12:31 +0800

    basic structures

---
+++ API/Structures.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QQSAPI
+{
+    public struct Note
+    {
+        public byte Key;
+        public ushort Track;
+        public uint Start, End;
+    }
+    public struct Tempo
+    {
+        public uint Tick;
+        public uint Value;
+    }
+}
